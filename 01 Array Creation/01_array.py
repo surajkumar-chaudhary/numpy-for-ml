15fb34845d403407c00d818038c33ff2053e15aa
Suraj Chaudhary <chaudharysuraz999@gmail.com>
2026-06-02 14:59:25 +0545

    Learn NumPy zeros and ones arrays

--- 01 Array Creation/01_array.py
+++ 01 Array Creation/01_array.py
@@ -2,4 +2,8 @@ import numpy as np
 
 arr = np.array([1, 2, 3, 4, 5])
 
-print(arr)
+print("Array:", arr)
+print("Shape:", arr.shape)
+print("Dimensions:", arr.ndim)
+print("Size:", arr.size)
+print("Data Type:", arr.dtype)
---
+++ 01 Array Creation/02_zeros_ones.py
@@ -0,0 +1,5 @@
+import numpy as np
+zeros = np.zeros(5)
+ones = np.ones(5)
+print("Zeros:", zeros)
+print("ones:", ones)
