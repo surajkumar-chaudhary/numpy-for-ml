fb5711619d4bd95d4b310feafe8b87621b1bb23f
Suraj Chaudhary <chaudharysuraz999@gmail.com>
2026-06-04 15:23:36 +0545

    Learn NumPy scalar broadcasting

---
+++ 04 Broadcasting/01_scalar_broadcasting.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([5, 10, 15, 20])
+
+print("Original:", arr)
+
+print("Add 10:", arr + 100)
+print("Subtract 5:", arr - 5)
+print("Multiply by 3:", arr * 3)
+print("Divide by 2:", arr / 2)
