5c1dd8265706e9e38907ebaf2c40d6f2807d7b9c
Suraj Chaudhary <chaudharysuraz999@gmail.com>
2026-06-04 15:37:25 +0545

    Learn NumPy column broadcasting

---
+++ 04 Broadcasting/03_column_broadcasting.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+matrix = np.array([
+    [1,2,3],
+    [4,5,6]
+])
+
+column = np.array([
+    [10],
+    [20]
+])
+
+print(matrix)
+print()
+print(column)
+print()
+print(matrix + column)
