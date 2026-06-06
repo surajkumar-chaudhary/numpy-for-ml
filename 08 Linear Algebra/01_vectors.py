8f358cf0870e520479cea88c8be6b80aeafcc3cc
Suraj Chaudhary <chaudharysuraz999@gmail.com>
2026-06-06 19:01:22 +0545

    Learn NumPy vectors

---
+++ 08 Linear Algebra/01_vectors.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+v1 = np.array([1, 2, 3])
+
+print(v1)
+print(v1.shape)
+print(v1.ndim)
+
+row = np.array([[10,20,30]])
+col = np.array([[10],
+                [20],
+                [30]])
+
+print(row.shape)
+print(col.shape)
